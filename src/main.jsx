120698c6ada0f6e8b3ae4be970269e89c65b6b1c
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-04 10:50:59 +0600

    Fixed tanstack query

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Main from "./routes/Main.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Main />
+    <QueryClientProvider client={queryClient}>
+      {/* Your app components */}
+      <Main />
+    </QueryClientProvider>
   </StrictMode>
 );
