fc8300a35a85e7913aebe0eb66c08a84a91b3ac8
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-05 17:04:33 +0600

    Added robust section

--- src/components/RobustSection.jsx
+++ src/components/RobustSection.jsx
@@ -1,7 +1,17 @@
 const RobustSection = () => {
   return (
-    <div className="">
+    <div className="relative">
       <img src="/RobustImage.avif" alt="Data Center" />
+      <div className="absolute text-center top-20 left-40">
+        <h1 className="text-[32px] font-semibold mb-2">
+          Robust data security and compliance
+        </h1>
+        <p className="text-[#646A73] mb-8">
+          Protecting customer data security is a top priority and the foundation
+          of our relationship with our customers.
+        </p>
+        <img src="/RobustBrandImg.avif" alt="Brand Icons" />
+      </div>
     </div>
   );
 };
