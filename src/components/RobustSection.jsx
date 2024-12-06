87e1638e6c353e06916cae3c1697078c0dc50183
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-06 14:28:27 +0600

    Made robust section responsive

--- src/components/RobustSection.jsx
+++ src/components/RobustSection.jsx
@@ -1,12 +1,12 @@
 const RobustSection = () => {
   return (
     <div className="relative">
-      <img src="/RobustImage.avif" alt="Data Center" />
-      <div className="absolute text-center top-20 left-40">
-        <h1 className="text-[32px] font-semibold mb-2">
+      <img src="/RobustImage.avif" alt="Data Center" className="md:h-auto h-80" />
+      <div className="absolute text-center md:top-0 lg:top-20 lg:p-0 md:p-2 lg:left-40 top-0">
+        <h1 className="md:text-[32px] text-xl font-semibold mb-2">
           Robust data security and compliance
         </h1>
-        <p className="text-[#646A73] mb-8">
+        <p className="text-[#646A73] mb-1 md:mb-8">
           Protecting customer data security is a top priority and the foundation
           of our relationship with our customers.
         </p>
