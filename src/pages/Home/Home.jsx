7f4271fa61bf05dc96155a1aaa73631dae7ec280
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-07 15:43:20 +0600

    Added performance section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import AccordionCard from "../../components/AccordionCardComponent";
+import LarkPerformance from "../../components/LarkPerformance";
 import PayLess from "../../components/PayLess";
 import PricingComponent from "../../components/PricingComponent";
 import RobustSection from "../../components/RobustSection";
@@ -6,6 +7,7 @@ import RobustSection from "../../components/RobustSection";
 const Home = () => {
   return (
     <>
+      <LarkPerformance />
       <PricingComponent />
       <RobustSection />
       <AccordionCard />
