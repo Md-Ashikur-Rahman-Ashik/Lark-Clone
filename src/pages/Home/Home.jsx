d1652b8b1c12f98dadd98c94c8bae3fd0dbb7d5b
Md-Ashikur-Rahman-Ashik <md.ashikur.upwork@gmail.com>
2024-12-07 14:53:03 +0600

    Added tailored section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import AccordionCard from "../../components/AccordionCardComponent";
 import PayLess from "../../components/PayLess";
+import PricingComponent from "../../components/PricingComponent";
 import RobustSection from "../../components/RobustSection";
 
 const Home = () => {
   return (
     <>
+      <PricingComponent />
       <RobustSection />
       <AccordionCard />
       <PayLess />
