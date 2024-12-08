b7de769024f7e975bad0838c7055d0c65d225c34
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-12-08 07:25:18 +0000

    Fixed merge conflict

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,5 @@
 const Home = () => {
-<<<<<<< HEAD
   return <div></div>;
-=======
-  return <div>This is homepage</div>;
->>>>>>> bb776b6d304428d56c947be09eb975c32d8207a8
 };
 
 export default Home;
